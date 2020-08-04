a03c41203e56f033dee6e7f29baa9eac54cbda47
jyu98-1661808 <jinayu@Jinas-MacBook-Pro.local>
2020-08-04 15:40:08 -0700

    Changes in package.json?

---
+++ build/precache-manifest.e766c34d4793ece2c59c52ea8f303eed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e61122254d3ce6283007f50adb829d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5dd8c92e9a5b86288f92",
+    "url": "/portfolio/static/css/main.016593c9.chunk.css"
+  },
+  {
+    "revision": "747c6a4bdc5256cc45e9",
+    "url": "/portfolio/static/js/2.04130711.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/portfolio/static/js/2.04130711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dd8c92e9a5b86288f92",
+    "url": "/portfolio/static/js/main.29928674.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "229dfdbfb30b08fc95e0bdd4a246741b",
+    "url": "/portfolio/static/media/green-arrow.229dfdbf.png"
+  }
+]);
