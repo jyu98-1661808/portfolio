19d25154f6283356399677ca0f5028e2db305a32
jyu98-1661808 <jinayu@Jinas-MacBook-Pro.local>
2020-08-04 18:30:22 -0700

    Updates

---
+++ precache-manifest.4add1a59803d17a34877f6e1ae4f483b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fc63bf6aeb4843be5090842cfa9047",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39784c8eba61525dbfb6",
+    "url": "/static/css/main.5bbf1aa4.chunk.css"
+  },
+  {
+    "revision": "747c6a4bdc5256cc45e9",
+    "url": "/static/js/2.04130711.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.04130711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39784c8eba61525dbfb6",
+    "url": "/static/js/main.77b910af.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "229dfdbfb30b08fc95e0bdd4a246741b",
+    "url": "/static/media/green-arrow.229dfdbf.png"
+  }
+]);
